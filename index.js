51e67f218782d3b52e9173e299aaffe806ed792b
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-09-10 18:39:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const ForgotPasswordRequest = require("./model/ForgotPasswordRequests");
 const DownloadExpensesList = require("./model/DownloadedExpenseList");
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')))
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "access.log"),
   {
